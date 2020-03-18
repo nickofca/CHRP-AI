6274ce72d3d5e056e287c95b06500d2cc4ead609
Jon Samuel <samueljon@Jons-MacBook-Pro.local>
2020-03-18 12:24:41 -0700

    Class Schedule

--- DataAcquisition/Class Schedule/Scripts/Class_Schedule.py
+++ DataAcquisition/Class Schedule/Scripts/Class_Schedule.py
@@ -8,20 +8,18 @@ Created on Wed Mar 11 15:34:41 2020
 
 import pandas as pd
 
-Class_schedule_2015_2022 = pd.read_excel('/Users/samueljon/Desktop/Desktop Folder/University of Arizona/Spring 2020/CHEE 443/Schedule (UofA).xlsx', index_col=0, sheet_name = '2015-2022')
-#Class_schedule_2015_2022.to_csv('Class Schedule 2015-2022.csv', index = False)
-
 class classschedule(object):
     
-    def __init__(self, date_start = '2015-01-01', date_end = '2015-02-01'):
+    def __init__(self, date_start = '2015-01-01', date_end = '2015-02-01', file = '/Users/samueljon/Desktop/Desktop Folder/University of Arizona/Spring 2020/CHEE 443/CHRP-AI/DataAcquisition/Class Schedule/Class Schedule 2015-2022.csv'):
         self.date_start = date_start
         self.date_end = date_end
+        self.data = pd.read_csv(file, index_col = 0)
         
     def schedule(self):
-        Schedule_Range = Class_schedule_2015_2022.loc[self.date_start:self.date_end]
+        Schedule_Range = self.data.loc[self.date_start:self.date_end]
         return Schedule_Range
 
 if __name__ == '__main__':
-    cs = classschedule(date_start = '2015-01-01', date_end = '2016-01-01')
+    cs = classschedule(date_start = '2015-01-01', date_end = '2017-01-01', file = '/Users/samueljon/Desktop/Desktop Folder/University of Arizona/Spring 2020/CHEE 443/CHRP-AI/DataAcquisition/Class Schedule/Class Schedule 2015-2022.csv')
     class_schedule = cs.schedule()
         
